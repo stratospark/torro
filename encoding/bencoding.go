75a13fbd1e17503679ef59c5332281bb7d0fc5a2
Patrick Rodriguez <patr84@gmail.com>
2015-08-20 19:50:30 -0700

    Implementing Integer decoding + tests.

--- encoding/bencoding.go
+++ encoding/bencoding.go
@@ -1,14 +1,17 @@
 package encoding
+
 import (
-	"strings"
-	"strconv"
 	"errors"
+	"strconv"
+	"strings"
 )
 
-var ErrDecodeStringBadLength = errors.New("Given length does not match length of string")
+var ErrDecodeStringBadLength = errors.New("Given length does not match length of string.")
+var ErrDecodeIntegerBadFormat = errors.New("Integer must begin with 'i' and end with 'e'.")
+var ErrDecodeIntegerNoPadding = errors.New("Integer must not be padded with zeroes (0).")
 
 func DecodeString(s string) (result string, err error) {
-	parts := strings.SplitN(s, ":", -1)
+	parts := strings.SplitN(s, ":", 2)
 	length, err := strconv.ParseInt(parts[0], 10, 32)
 
 	if err != nil {
@@ -24,5 +27,22 @@ func DecodeString(s string) (result string, err error) {
 		}
 	}
 	return result, err
-
 }
+
+func DecodeInteger(s string) (result int64, err error) {
+	if strings.HasPrefix(s, "i") && strings.HasSuffix(s, "e") {
+		integerString := s[1:len(s)-1]
+		if strings.HasPrefix(integerString, "0") && len(integerString) > 1 {
+			return 0, ErrDecodeIntegerNoPadding
+		}
+		result, err = strconv.ParseInt(integerString, 10, 32)
+		if err != nil {
+			return 0, err
+		}
+	} else {
+		result = 0
+		err = ErrDecodeIntegerBadFormat
+	}
+	return result, err
+}
+
--- encoding/bencoding_test.go
+++ encoding/bencoding_test.go
@@ -1,21 +1,34 @@
 package encoding
 
 import (
+	"fmt"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 func TestDecodeString(t *testing.T) {
 	Convey("When given a valid input", t, func() {
-		Convey("Decoding '4:blah' should return 'blah'.", func() {
+		Convey("Decoding '4:blah' returns 'blah'.", func() {
 			result, err := DecodeString("4:blah")
 			So(result, ShouldEqual, "blah")
 			So(err, ShouldBeNil)
 		})
 
-		Convey("Decoding '0:' should return ''", func() {
+		Convey("Decoding '0:' returns empty string.", func() {
 			result, err := DecodeString("0:")
 			So(result, ShouldEqual, "")
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Decoding '10:asdfghjklq' works. 2 digit length.", func() {
+			result, err := DecodeString("10:asdfghjklq")
+			So(result, ShouldEqual, "asdfghjklq")
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Decoding '15:this:has:colons' works.", func() {
+			result, err := DecodeString("15:this:has:colons")
+			So(result, ShouldEqual, "this:has:colons")
 			So(err, ShouldBeNil)
 		})
 	})
@@ -32,6 +45,53 @@ func TestDecodeString(t *testing.T) {
 			So(result, ShouldEqual, "")
 			So(err, ShouldNotBeNil)
 		})
-
 	})
+}
+
+type InputResultErr struct {
+	input  interface{}
+	result interface{}
+	hasError bool
+	err    error
+
+}
+
+func TestDecodeInteger(t *testing.T) {
+	Convey("Given a valid input", t, func() {
+		cases := map[string]InputResultErr{
+			"'i3e' returns 3": InputResultErr{"i3e", 3, false, nil},
+			"'i-1e' returns -1": InputResultErr{"i-1e", -1, false, nil},
+			"'i100e' returns 100": InputResultErr{"i100e", 100, false, nil},
+			"'i0e' returns 0": InputResultErr{"i0e", 0, false, nil},
+		}
+		for description, test := range cases {
+			Convey(fmt.Sprintf("%s", description), func() {
+				input := test.input.(string)
+				result, err := DecodeInteger(input)
+				So(result, ShouldEqual, test.result)
+				So(err, ShouldEqual, test.err)
+			})
+		}
+	})
+
+	Convey("Given an invalid input", t, func() {
+		cases := map[string]InputResultErr{
+			"'i04e'": InputResultErr{"i04e", 0, true, ErrDecodeIntegerNoPadding},
+			"'iae'": InputResultErr{"iae", 0, true, nil},
+			"'e9a": InputResultErr{"e9a", 0, true, ErrDecodeIntegerBadFormat},
+		}
+		for description, test := range cases {
+			Convey(fmt.Sprintf("%s", description), func() {
+				input := test.input.(string)
+				result, err := DecodeInteger(input)
+				So(result, ShouldEqual, test.result)
+				if test.hasError && test.err != nil {
+				So(err, ShouldEqual, test.err)
+				} else {
+					So(err, ShouldNotBeNil)
+				}
+			})
+		}
+	})
+
 }
