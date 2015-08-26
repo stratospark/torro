a302b425f780f5316015ab48e145ed8c9a443540
Patrick Rodriguez <patr84@gmail.com>
2015-08-25 19:45:53 -0700

    Pretty print the parsed torrent file.

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/stratospark/torro/bencoding"
 	"io/ioutil"
+	"github.com/kr/pretty"
 )
 
 func main() {
@@ -18,10 +18,15 @@ func main() {
 	lex := bencoding.BeginLexing(".torrent", torrentStr, bencoding.LexBegin)
 	tokens := bencoding.Collect(lex)
 
-	tokenCounts := make(map[bencoding.Token]int)
-	for _, t := range tokens {
-		tokenCounts[t]++
-		fmt.Println(t)
-	}
-	fmt.Println(tokenCounts)
+	output := bencoding.Parse(tokens)
+
+//	reuslt := output.Output.(map[string]interface{})
+
+	pretty.Print(output.Output)
+//	tokenCounts := make(map[bencoding.Token]int)
+//	for _, t := range tokens {
+//		tokenCounts[t]++
+//		fmt.Println(t)
+//	}
+//	fmt.Println(tokenCounts)
 }
