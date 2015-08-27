e1734a41634cb3c5bfbe9019bfbc472c57277ec3
Patrick Rodriguez <patr84@gmail.com>
2015-08-27 14:57:08 -0700

    Fleshed out TrackerRequest fields.

--- structure/tracker.go
+++ structure/tracker.go
@@ -1,8 +1,24 @@
 package structure
 
 type TrackerRequest struct {
+	InfoHash   string
+	PeerID     string
+	Port       int
+	Uploaded   int
+	Downloaded int
+	Left       int
+	Compact    bool
+	NoPeerID   bool
+	Event      string
+	IP         string
+	NumWant    int
+	Key        string
+	TrackerID  string
 }
 
-func NewTrackerRequest() *TrackerRequest {
-	return &TrackerRequest{}
+func NewTrackerRequest(metainfo *Metainfo) *TrackerRequest {
+	return &TrackerRequest{
+		InfoHash: metainfo.Info.Hash,
+		PeerID:   "-TR2840-nj5ovtkoz2ed8", // TODO: generate unique PeerID
+	}
 }
--- structure/tracker_test.go
+++ structure/tracker_test.go
@@ -7,7 +7,12 @@ import (
 
 func TestTrackerRequest(t *testing.T) {
 	Convey("Creating a Tracker Request struct", t, func() {
-		request := NewTrackerRequest()
+		filename := "../testfiles/TheInternetsOwnBoyTheStoryOfAaronSwartz_archive.torrent"
+		metainfo := NewMetainfo(filename)
+		request := NewTrackerRequest(metainfo)
 		So(request, ShouldNotBeNil)
+		So(request.InfoHash, ShouldEqual, "%29%eb%26%d6%ba%89d%9c%10%5d%c8%e2~%af%dc%0c.%f6%22%92")
+		So(request.PeerID, ShouldEqual, "-TR2840-nj5ovtkoz2ed8")
+
 	})
 }
