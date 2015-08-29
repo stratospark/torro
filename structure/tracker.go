074fd846dc3aaf26e066b5e415bb4afb4a17344b
Patrick Rodriguez <patr84@gmail.com>
2015-08-28 20:18:44 -0700

    Implemented parsing of TrackerResponse (including binary peer list) from hex response.

--- structure/tracker.go
+++ structure/tracker.go
@@ -1,6 +1,9 @@
 package structure
 
 import (
+	"encoding/binary"
+	"github.com/stratospark/torro/bencoding"
+	"net"
 	"strconv"
 )
 
@@ -55,4 +58,47 @@ func (request *TrackerRequest) GetURL() string {
 		"&event=" + request.Event
 
 	return url
+}
+
+type Peer struct {
+	IP   net.IP
+	Port uint16
+}
+
+type TrackerResponse struct {
+	Complete    int
+	Incomplete  int
+	Downloaded  int
+	Interval    int
+	MinInterval int
+	Peers       []Peer
+}
+
+func NewTrackerResponse(responseStr string) *TrackerResponse {
+	lex := bencoding.BeginLexing("response", responseStr, bencoding.LexBegin)
+	tokens := bencoding.Collect(lex)
+	parser := bencoding.Parse(tokens)
+	o := parser.Output.(map[string]interface{})
+	//	fmt.Println(o)
+
+	// TODO: Handle required/optional fields
+	response := &TrackerResponse{}
+	addIntField(&response.Complete, o["complete"], true)
+	addIntField(&response.Incomplete, o["incomplete"], true)
+	addIntField(&response.Downloaded, o["downloaded"], false)
+	addIntField(&response.Interval, o["interval"], true)
+	addIntField(&response.MinInterval, o["min interval"], false)
+
+	// TODO: Handle dictionary vs binary peer models
+	peers := make([]Peer, 0)
+	peerBytes := o["peers"].([]byte)
+	for i := 0; i < len(peerBytes); i += 6 {
+		ip := net.IPv4(peerBytes[i+3], peerBytes[i+2], peerBytes[i+1], peerBytes[i])
+
+		port := binary.BigEndian.Uint16(peerBytes[i+4 : i+6])
+		peers = append(peers, Peer{IP: ip, Port: port})
+	}
+	response.Peers = peers
+
+	return response
 }
